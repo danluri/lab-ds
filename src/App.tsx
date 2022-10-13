8181e40649db356a50c8f0fbf91ed4d9fde3c780
Danilo Luque Ribeiro <danilo.ribeiro@newgo.com.br>
2022-10-13 18:27:01 -0300

    Teste commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import './styles/global.css';
+
+export function App() {
+
+  return (
+    <h1 className="font-bold text-2xl text-gray-900">Hello World</h1>
+  )
+}
+
+//export default App
+// permite exportar com qq nome, não é uma boa prática
